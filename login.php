a46a5f4c9c6a7b7d6db7e7d1acdc65858f5a7558
MarcJefferson Sumilang <sumilangmarc1999@gmail.com>
2021-06-28 11:10:28 +0800

    use window location to redirect

--- login.php
+++ login.php
@@ -49,11 +49,9 @@ body {
               $_SESSION['user_info'] = $row;
             
               if($row['sumilang_type'] == "admin"){
-                header('location: http://sumilangmarc.com/admin/admin_home.php');
-                exit;
+                echo ("<script>location.href=http://sumilangmarc.com/admin/admin_home.php</script>");
               }else{
-                header('location: http://sumilangmarc.com/user/user_home.php');
-                exit;
+                echo ("<script>location.href=http://sumilangmarc.com/user/user_home.php</script>");
               }
             }else{
               echo "<script>alert('Wrong login details.')</script>";
