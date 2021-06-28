33ed6a9ff211889e674bb76ffe0c242d99d9adb3
MarcJefferson Sumilang <sumilangmarc1999@gmail.com>
2021-06-28 14:48:17 +0800

    logout

--- user/user_home.php
+++ user/user_home.php
@@ -47,6 +47,7 @@
         <!-- <h1><a href="index.html">The<span>Event</span></a></h1>-->
         <a href="index.html" class="scrollto"><img src="assets/img/logo.png" alt="touch-icon" title=""></a>
         <button> <a href="forum/index.php">forum</a></button>
+        <a href="../login.php" onclick="<?php session_destroy();?>">Logout</a>
         <button name="logout" type="submit" class="btn btn-danger my-2">Logout</button>
     <?php
                 if(isset($_POST['logout'])){
